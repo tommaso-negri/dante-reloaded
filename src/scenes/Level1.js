53bdd1aa16679f1e775c2df5d60b713b4fdbfaba
Tommaso Negri <tommaso.negri98@gmail.com>
2018-11-09 19:51:32 +0100

    Tile Test

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -3,7 +3,6 @@ import Phaser from 'phaser';
 let player
 let platforms
 let cursors
-let map
 
 export default class Level1 extends Phaser.Scene {
   constructor() {
@@ -11,27 +10,34 @@ export default class Level1 extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet('player', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('player', 'assets/dude.png', {
+    frameWidth: 32, frameHeight: 48 });
     this.load.image('platform', 'assets/platform.png');
     this.load.image('sky', 'assets/sky.png');
 
-    this.load.image('tiles', 'assets/super_mario.png');
-		this.load.tilemapTiledJSON('map', 'assets/maps/Level1Map.json');
+    this.load.image('tiles1', 'assets/super_mario.png');
+		this.load.tilemapTiledJSON('map1', 'assets/maps/Level1Map.json');
   }
 
   create() {
     this.add.image(0, 0, 'sky').setOrigin(0, 0);
 
-    this.map = this.add.tilemap('map');
-    this.map.addTilesetImage('super_mario','tiles');
-    this.backgroundLayer = this.map.createLayer('backgroundLayer');	
+    let map1 = this.make.tilemap({ key: 'map1' });
+    let tileset1 = map1.addTilesetImage('SuperMarioBros-World1-1', 'tiles1');
+    let layer1 = map1.createStaticLayer('World1', tileset1, 0, 0);
 
-    // platforms = this.physics.add.staticGroup();
+    cursors = this.input.keyboard.createCursorKeys();
 
-    // platforms.create(400, 568, 'platform').setScale(2).refreshBody();
-    // platforms.create(600, 400, 'platform');
-    // platforms.create(50, 250, 'platform');
-    // platforms.create(750, 220, 'platform');
+    let controlConfig = {
+      camera: this.cameras.main,
+      left: cursors.left,
+      right: cursors.right,
+      speed: 0.5
+    };
+
+    let controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+
+    this.cameras.main.setBounds(0, 0, layer1.x + layer1.width, 0);
 
     player = this.physics.add.sprite(80, 196, 'player');
     player.setCollideWorldBounds(true);
@@ -56,12 +62,10 @@ export default class Level1 extends Phaser.Scene {
         frameRate: 10,
         repeat: -1
     });
-
-    cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
-    this.physics.add.collider(player, platforms);
+    // this.physics.add.collider(player, platforms);
 
     player.body.velocity.x = 0;
     if (cursors.left.isDown)
